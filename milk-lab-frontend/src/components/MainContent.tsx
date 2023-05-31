c0e7f9fe5c831cbba69539af0e9b83b052503d41
R-Ravantti <ravantti97@gmail.com>
2023-05-31 16:43:08 +0200

    Components for main page and api calls used

--- milk-lab-frontend/src/components/MainContent.tsx
+++ milk-lab-frontend/src/components/MainContent.tsx
@@ -1,6 +1,19 @@
 import { useState, useEffect } from "react";
 import { Milk, fetchAllMilks } from "@/apicalls/milkfetcher";
+import MilkCard from "./MilkCard";
 
 export default function MainContent() {
     const [milks, setMilks] = useState<Milk[]>([]);
+
+    useEffect(() => {
+        fetchAllMilks(setMilks);
+    }, []);
+
+    return (
+        <div>
+            {milks.map(milk => {
+                return <MilkCard milk={milk}/>
+            })}
+        </div>
+    )
 }
--- milk-lab-frontend/src/components/MilkCard.tsx
+++ milk-lab-frontend/src/components/MilkCard.tsx
@@ -1,3 +1,14 @@
-export default function MilkCard() {
-    
+import { Milk } from "@/apicalls/milkfetcher";
+
+type MilkCardProps = {
+    milk: Milk
+}
+
+export default function MilkCard(props: MilkCardProps) {
+    const {milk} = props;
+    return (
+        <div>
+            <p>{milk.name}</p>
+        </div>
+    )
 }
