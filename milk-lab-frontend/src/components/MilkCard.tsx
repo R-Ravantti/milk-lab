8867d5757526567633929f6af5167e468366c511
R-Ravantti <ravantti97@gmail.com>
2023-06-01 14:26:16 +0200

    begun work on tailwind css for styling

--- milk-lab-frontend/src/components/MilkCard.tsx
+++ milk-lab-frontend/src/components/MilkCard.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import React from "react";
 import { Milk } from "@/apicalls/milkfetcher";
 
 type MilkCardProps = {
@@ -6,11 +6,15 @@ type MilkCardProps = {
 }
 
 export default function MilkCard(props: MilkCardProps) {
-    const {milk} = props;
+    const { milk } = props;
     return (
-        <div>
-            <p>{milk.name}</p>
-            <Link href={"/" + milk.id}>Link to milk</Link>
-        </div>
+        <a className="w-72 rounded overflow-hidden shadow-lg mx-10 my-8" href={"/" + milk.id}>
+            <img className="w-full" src="/milk.png"></img>
+            <p className="font-bold text-l m-1 break-normal">{milk.name}</p>
+            <div className="flex justify-between">
+                <p className="m-1 text-gray-400">{milk.type}</p>
+                <p className="m-1 text-lime-700">{milk.storage} liter</p>
+            </div>
+        </a>
     )
 }
--- milk-lab-frontend/src/pages/index.tsx
+++ milk-lab-frontend/src/pages/index.tsx
@@ -1,12 +1,27 @@
-import MainContent from "@/components/MainContent";
+import React, { useState, useEffect } from "react";
+import { Milk, fetchAllMilks } from "@/apicalls/milkfetcher";
+import MilkCard from "@/components/MilkCard";
+
+function getTypes(milks: Milk[]) {
+  return Array.from(new Set(milks.map(milk => milk.type)));
+}
 
 export default function Home() {
+  const [milks, setMilks] = useState<Milk[]>([]);
+
+  useEffect(() => {
+    fetchAllMilks(setMilks);
+  }, []);
   return (
     <main>
       <header>
 
       </header>
-      <MainContent/>
+      <div className="flex flex-wrap justify-center">
+        {milks.map(milk => {
+          return <MilkCard milk={milk} />
+        })}
+      </div>
     </main>
   )
 }
